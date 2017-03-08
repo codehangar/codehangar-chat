e83b471fc81b795080a1851c44d49d2ac8eb69d7
C-Martha <marthacornejo2@gmail.com>
2017-03-07 23:35:18 -0500

    (in progress: slider): started working on sliders with react bootstrap library

--- react/src/gif-controls/gif-controls.js
+++ react/src/gif-controls/gif-controls.js
@@ -2,21 +2,42 @@
  * Created by Valetery on 3/3/2017.
  */
 import React from 'react';
-import {connect} from 'react-redux';
-import {Panel, ListGroup, ListGroupItem} from 'react-bootstrap';
+import { connect } from 'react-redux';
+import { Panel, ListGroup, ListGroupItem } from 'react-bootstrap';
+import ReactBootstrapSlider from 'react-bootstrap-slider';
 
-const gifControls = () => {
-    return (
-        <Panel collapsible defaultExpanded header="Control Panel">
-            Control Panel
+//working on sliders from library: 
+// https://www.npmjs.com/package/react-bootstrap-slider
+class gifControls extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.valueChanged = this.valueChanged.bind(this);
+    }
+    render() {
+        return (
+            <Panel collapsible defaultExpanded header="Control Panel">
+                Control Panel
             <ListGroup fill>
-                <ListGroupItem>Item 1</ListGroupItem>
-                <ListGroupItem>Item 2</ListGroupItem>
-                <ListGroupItem>&hellip;</ListGroupItem>
-            </ListGroup>
-            Some more panel content here.
+                    <ListGroupItem>
+                        <ReactBootstrapSlider
+                            value="3"
+                            step={this.valueChanged}
+                            max="10"
+                            min="0"
+                            orientation="vertical"
+                            reversed="true" />
+                    </ListGroupItem>
+                </ListGroup>
+         
         </Panel>
-    );
-};
+        );
+    }
+
+    valueChanged(event) {
+        this.props.onChange(this.props.value, event.target.value);
+    }
+
+}
 
 export default connect()(gifControls);
